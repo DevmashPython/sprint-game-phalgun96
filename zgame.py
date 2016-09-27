7041d838cdaa027c7723938456c2c2c4b99b106a
@phalgun96! <shivaphalgun96@gmail.com>
2016-09-27 18:16:41 +0530

    zgame

---
+++ zgame.py
@@ -0,0 +1,41 @@
+import time
+import msvcrt
+
+finish=30
+f1=10
+f2=25
+count=0
+
+print "press enter to start"
+raw_input()
+s_time=time.time()
+
+while 1:
+    key=msvcrt.getch()
+    if key=='d':
+    	count+=1
+    	print "--->"
+    if count==f1:
+    	print "turn down."
+    	break
+
+while 1:
+    key1=msvcrt.getch()
+    if key1=='s':
+    	count+=1
+    	print"              "
+    	print"             |"
+    	print"             v"
+    if count==f2:
+    	print"               turn right.",
+
+ while 1:
+    key2=='d':
+    	count+=1
+    	print"-->"
+    if 	count=finish:
+    	break
+
+    time_elapsed=time,time()-s_time
+    	print "game over"
+    	print "time taken: " str(time_elapsed)
